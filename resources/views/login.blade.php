7f3d54f10bc3505e48d80219269c53587b5051ad
rcero3 <bryanneciosup626@gmail.com>
2023-09-14 01:56:51 -0500

    css en lara

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="author" content="AcademyView">
     <meta name="description" content="Esta es una página web de visualizacion academica">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    {{--<link rel="stylesheet" href="{{asset('css/app.css')}}">--}}
+    <link rel="stylesheet" href="https://sistemawebsr-production.up.railway.app/build/assets/app-d10f5d14.css">
     <title>Login</title>
 </head>
 <body>
